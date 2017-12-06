73dc1e7cedf8435bd4b81bdea1ca1a08ddb4b239
Zoltan <zolt0077@edu.eal.dk>
2017-12-06 20:17:32 +0100

    Message board basics - architecture improvement

---
+++ AISupport-View/application/components/MessageBoard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="messages">
+        <div v-for="message in messages">
+            <span v-text="message.sender"></span>: <span v-text="message.text"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'message-board',
+        data () {
+            return {
+                messages: []
+            }
+        },
+        methods: {
+            printMessage (sender, text) {
+                this.messages.push({
+                    sender: sender,
+                    text: text
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
